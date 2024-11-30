f466a37853d15564f6770601eeed483b0773996d
aarati01 <131001442+aarati01@users.noreply.github.com>
2024-11-30 10:49:12 -0500

    Setting up the model

--- model/employeeModel.js
+++ model/employeeModel.js
@@ -1,10 +1,10 @@
-import mongoose from 'mongoose';
-const {schema} =mongoose;
-const employeeeSchema=new Schema({
-    f_name:String,
-    l_name:String,
-    emp_id:Number,
-    salary:Number,
-    working_dep:String,
-    
-})
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+const employeeeSchema = new Schema({
+  f_name: String,
+  l_name: String,
+  emp_id: Number,
+  salary: Number,
+  working_dep: String,
+  email: String,
+});
